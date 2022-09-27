61169fe16f84a1ee693ebba7cd9dc2d796e3ea38
jamjamjon <51357717+jamjamjon@users.noreply.github.com>
2022-09-27 22:29:02 +0800

    Update torch_utils.py

--- utils/torch_utils.py
+++ utils/torch_utils.py
@@ -57,9 +57,8 @@ def device_count():
 
 
 def select_device(device='', batch_size=0, newline=True):
-    # device = None or 'cpu' or 0 or '0' or '0,1,2,3'
-    # s = f'Enviroment 🚀 {file_date()} | Python-{platform.python_version()} | torch-{torch.__version__} | '
-    s = f'Enviroment 🚀 Python-{platform.python_version()} | Torch-{torch.__version__} | '
+    # # device = None or 'cpu' or 0 or '0' or '0,1,2,3'
+    s = f'Enviroment ✨ Python-{platform.python_version()} | Torch-{torch.__version__} | '
     device = str(device).strip().lower().replace('cuda:', '').replace('none', '')  # to string, 'cuda:0' to '0'
     cpu = device == 'cpu'
     mps = device == 'mps'  # Apple Metal Performance Shaders (MPS)
@@ -70,7 +69,7 @@ def select_device(device='', batch_size=0, newline=True):
         assert torch.cuda.is_available() and torch.cuda.device_count() >= len(device.replace(',', '')), \
             f"Invalid CUDA '--device {device}' requested, use '--device cpu' or pass valid CUDA device(s)"
 
-    if not cpu and torch.cuda.is_available():  # prefer GPU if available
+    if not cpu and not mps and torch.cuda.is_available():  # prefer GPU if available
         devices = device.split(',') if device else '0'  # range(torch.cuda.device_count())  # i.e. 0,1,6,7
         n = len(devices)  # device count
         if n > 1 and batch_size > 0:  # check batch_size is divisible by device_count
@@ -78,19 +77,18 @@ def select_device(device='', batch_size=0, newline=True):
         space = ' ' * (len(s) + 1)
         for i, d in enumerate(devices):
             p = torch.cuda.get_device_properties(i)
-            s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / (1 << 20):.0f}MiB)"  # bytes to MB
+            s += f"{'' if i == 0 else space}CUDA:{d} ({p.name}, {p.total_memory / (1 << 20):.0f}MiB)\n"  # bytes to MB
         arg = 'cuda:0'
-    elif not cpu and getattr(torch, 'has_mps', False) and torch.backends.mps.is_available():  # prefer MPS if available
-        s += 'MPS'
+    elif mps and getattr(torch, 'has_mps', False) and torch.backends.mps.is_available():  # prefer MPS if available
+        s += 'MPS\n'
         arg = 'mps'
     else:  # revert to CPU
-        s += 'CPU'
+        s += 'CPU\n'
         arg = 'cpu'
 
     if not newline:
         s = s.rstrip()
-    LOGGER.info(s.encode().decode('ascii', 'ignore') if platform.system() == 'Windows' else colorstr(f'{s}'))  # emoji-safe
-    
+    LOGGER.info(colorstr(f'{s}'))
     return torch.device(arg)
 
 
